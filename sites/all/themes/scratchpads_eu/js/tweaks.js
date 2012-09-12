ad3eeff012f1bb895e1304a2e40487d7f5668e99
Alice Heaton <a.heaton@nhm.ac.uk>
2012-09-12 13:11:02 +0100

    Applied feedback from meeting - applied fonts, slightly different colors, added background color, improved the js for moving the left hand side graphic

--- sites/all/themes/scratchpads_eu/js/tweaks.js
+++ sites/all/themes/scratchpads_eu/js/tweaks.js
@@ -1,17 +1,39 @@
 (function($) {
+  var fudge = 42;
+  var sidebar_image_height = 0;
+  var sidebar_height = 0;
+  
+  function move_sidebar_image() {
+    var sidebar = $('#region-sidebar-first div.region-inner');
+    var sidebar_pos = sidebar.offset();
+    var viewport_height = $(window).height();
+
+    var final_height = viewport_height - sidebar_pos.top - fudge;
+    if (final_height - sidebar_image_height < sidebar_height) {
+      final_height = sidebar_height + sidebar_image_height;
+    }
+    sidebar.find('div.menu-block-wrapper > ul.menu').css('min-height', final_height);
+  }
+  
+  function keep_sidebar_image_visible() {
+    var sidebar = $('#region-sidebar-first div.region-inner');
+    sidebar_height = sidebar.height();
+    
+    // Get the background image height
+    var img = new Image;
+    img.onload = function() {
+      sidebar_image_height = img.height;
+        
+      $(window).bind('scroll resize', move_sidebar_image);
+      move_sidebar_image();    	
+    };
+    
+    img.src = sidebar.find('div.menu-block-wrapper > ul.menu').css('background-image').replace(/"|url\(|\)$/ig, "");
+  }
+	
   // We use document.ready rather than behaviours as we only want
   // this to run once.
   $(document).ready(function() {
-	// Ensure sidebar-first is at least the height of the viewport
-	var sidebar = $('#region-sidebar-first div.region-inner');
-    var sidebar_pos = sidebar.offset();
-    var sidebar_size = {width: sidebar.width(), height: sidebar.height()};
-    var viewport_size = {width: $(window).width(), height: $(window).height()};
-    if (sidebar_pos.top + sidebar_size.height < viewport_size.height) {
-      //sidebar.css('min-height', viewport_size.height - sidebar_pos.top - 32);
-      sidebar.find('div.menu-block-wrapper > ul.menu, ul.facetapi-facetapi-links').css('min-height',
-    	  viewport_size.height - sidebar_pos.top - 42);
-    }
-    
+    keep_sidebar_image_visible();        
   });
 })(jQuery);
