8c09856875a87621e9dd7db98c9aed41fc09db71
Alice Heaton <a.heaton@nhm.ac.uk>
2012-09-14 17:41:22 +0100

    Finish the accordion functionality. Still needs different fields/themeing, but it's working otherwise.

--- sites/all/themes/scratchpads_eu/js/accordion.js
+++ sites/all/themes/scratchpads_eu/js/accordion.js
@@ -4,10 +4,50 @@
   function setup_accordion() {
     root = $('#block-views-nodequeue-1-block');
     
+    /* Add relevant classes now */
     $('div.views-row', root).addClass('accordion-collapsed');
     $('div.views-row-last', root)
     .removeClass('accordion-collapsed')
+    .removeClass('views-row-last')
     .addClass('accordion-expanded');
+
+    $('div.accordion-collapsed', root).live('click', function(e) {
+      /*
+      $('div.accordion-expanded', root)
+      .removeClass('accordion-expanded')
+      .addClass('accordion-collapsed');
+      
+      $(this)
+      .removeClass('accordion-collapsed')
+      .addClass('accordion-expanded');
+      */
+
+      /* Hide the expanded */
+      $('div.accordion-expanded div.accordion-empty').fadeIn('fast');
+      $('div.accordion-expanded div.accordion-background').fadeOut('fast');
+      $('div.accordion-expanded div.views-field-body').fadeOut('fast');
+      $('div.accordion-expanded div.views-field-view-node').fadeOut('fast');
+      
+      $('div.accordion-expanded', root).animate({
+        width: 150
+      }, 'fast', function() {
+        $(this).removeClass('accordion-expanded')
+        $(this).addClass('accordion-collapsed');
+      });
+
+      /* Show the collapsed */
+      $(this).find('div.accordion-empty').fadeOut('fast');
+      $(this).find('div.accordion-background').fadeIn('fast');
+      $(this).find('div.views-field-body').fadeIn('fast');
+      $(this).find('div.views-field-view-node').fadeIn('fast');
+      
+      $(this).animate({
+        width: 400
+      }, 'fast', function() {
+        $(this).addClass('accordion-expanded')
+        $(this).removeClass('accordion-collapsed');
+      });
+    });
   }
   
   /*
