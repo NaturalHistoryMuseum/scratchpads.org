ad049fb61fdc37c537b360d431cc58bc129ce824
Alice Heaton <a.heaton@nhm.ac.uk>
2012-09-24 16:49:29 +0100

    Implemented event manager

--- sites/all/themes/scratchpads_eu/templates/user-profile.tpl.php
+++ sites/all/themes/scratchpads_eu/templates/user-profile.tpl.php
@@ -1,4 +1,6 @@
 <?php 
   echo $user_profile['user_picture']['#markup'];
-  echo $user_profile['field_bio'][0]['#markup'];
+  if (isset($user_profile['field_bio'])) {
+    echo $user_profile['field_bio'][0]['#markup'];
+  }
 ?>
