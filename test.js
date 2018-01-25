69f94d48be1f4b7964dc7b518fb4a290fbcd42b1
Paul Kiddle <p.kiddle@nhm.ac.uk>
2018-01-25 18:46:40 +0000

    upgrade tester

--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 const childProcess = require("child_process");
 const http = require("http");
 const path = require("path");
+const readline = require("readline")
 const pa11y = require("pa11y");
-const htmlReporter = require("pa11y/reporter/html");
+const htmlReporter = require("pa11y-reporter-html");
 const glob = require("glob-promise");
 
 const jekyll = childProcess.spawn("npm", ["start"]);
@@ -10,20 +11,44 @@ const jekyll = childProcess.spawn("npm", ["start"]);
 jekyll.stdout.pipe(process.stdout);
 jekyll.stderr.pipe(process.stderr);
 
+let inLoop = false;
+let breakLoop = false;
+let cancel;
+const cancellation = new Promise((resolve, reject) => {
+  cancel = reject;
+})
+
+process.on('SIGINT', () => {
+  jekyll.kill();
+
+  if(inLoop) {
+    readline.clearLine(process.stdout);
+    cancel({ message: 'Tests killed' });
+    breakLoop = true;
+  } else {
+    process.exit();
+  }
+})
+
 jekyll.stdout.on("data", async buffer => {
   const [url] = String(buffer).match(/http:\/\/[0-9.:]+/) || "";
 
   if (url) {
+    jekyll.stdout.unpipe(process.stdout);
     const paths = await glob("**/*.html", {
       cwd: path.resolve(process.cwd(), "_site")
     });
 
-    const tester = pa11y({
+    const options = ({
       ignore: [
         "notice",
         "warning",
         "WCAG2AA.Principle1.Guideline1_4.1_4_3.G18.Fail",
-        "WCAG2AA.Principle1.Guideline1_4.1_4_3.G145.Fail"
+        "WCAG2AA.Principle1.Guideline1_4.1_4_3.G145.Fail",
+        // Redirect pages don't have title elements but I don't care
+        "WCAG2AA.Principle2.Guideline2_4.2_4_2.H25.1.NoTitleEl",
+        // Twitter generates a frame with no title
+        "WCAG2AA.Principle2.Guideline2_4.2_4_1.H64.1"
       ]
     });
 
@@ -32,23 +57,28 @@ jekyll.stdout.on("data", async buffer => {
     const results = [];
     const errors = [];
 
-    for (const path of paths) {
-      results.push(
-        ...(await new Promise((resolve, reject) =>
-          tester.run(url + "/" + encodeURI(path), (error, results) => {
-            if (error) {
-              errors.push(error);
+    inLoop = true;
 
-              return resolve([]);
-            }
+    for (const ix in paths) {
+      const path = paths[ix];
+      process.stdout.write(`${ix}/${paths.length} ${path}`);
+      readline.clearLine(process.stdout, 1)
+      readline.cursorTo(process.stdout, 0);
 
-            resolve(
-              results.map(r => ({ ...r, message: path + ": " + r.message }))
-            );
-          })
-        ))
-      );
+      try {
+        const { issues } = await Promise.race([pa11y(url + "/" + encodeURI(path), options), cancellation]);
+
+        results.push(...issues)
+      } catch (error) {
+        errors.push(error);
+      }
+
+      if(breakLoop) {
+        break;
+      }
     }
+
+    inLoop = false;
 
     errors.forEach(e => {
       console.log(e.message);
@@ -59,7 +89,14 @@ jekyll.stdout.on("data", async buffer => {
 
     jekyll.kill();
 
-    const html = htmlReporter.process(results, url);
+    if (results.length === 0) {
+      console.log('All executed tests passed!')
+      process.exit();
+    }
+
+    console.log(`${results.length} issues found`);
+
+    const html = await htmlReporter.results({ documentTitle: 'Scratchpads', pageUrl: url, issues: results });
 
     const server = http.createServer(function(req, res) {
       res.end(html);
